d918d704bcee47486a233b753c8d1be7ab329776
Matthias Taeschner <matthias.taeschner@uni-leipzig.de>
2025-10-17 12:00:30 +0200

    Preprocessor switch to uv

---
+++ preprocessor/README.md
@@ -0,0 +1,3 @@
+# CoLIBRi Preprocessor
+
+ToDo
--- preprocessor/entrypoint.sh
+++ preprocessor/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-exec conda run --no-capture-output -n preprocessor-env gunicorn \
+exec gunicorn \
   --bind "0.0.0.0:6201" \
   --timeout 600 \
   --chdir ./src/flask \
