3d8df9aabcce34035708f452ea4cd3174a886d93
Matthias Taeschner <matthias.taeschner@uni-leipzig.de>
2025-10-22 15:52:21 +0200

    Extend logging info

--- conv-search/src/flask/backend.py
+++ conv-search/src/flask/backend.py
@@ -8,7 +8,11 @@ from search_engine import EmbeddingSearchEngine, RemoteEmbeddingSearchEngine
 
 from flask import Flask
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s +0000] [%(process)d] [%(levelname)s] [%(filename)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 LOGGER = logging.getLogger(__name__)
 
 app = Flask(__name__)
